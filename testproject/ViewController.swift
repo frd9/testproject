cf38c4d66bc20665c2c373569bbacfd0e3334f2f
チャチャ <firdakhairunnisa@gmail.com>
2024-05-02 00:00:04 +0700

    initial setup

---
+++ testproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testproject
+//
+//  Created by チャチャ on 01/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
